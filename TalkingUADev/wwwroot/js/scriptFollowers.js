932b68cbe2ba7885c5c36df6ed1457f75b82c0bf
quareIT <dreksler.erik@gmail.com>
2023-05-10 17:07:15 +0300

    addingDesignForGettingFollowersLikedSubsBugFixed

---
+++ TalkingUADev/wwwroot/js/scriptFollowers.js
@@ -0,0 +1,8 @@
+let avatars = document.querySelectorAll('.nickname .story img');
+avatars.forEach((elem) => {
+    if (elem.width > elem.height) {
+        elem.style.height = '100%';
+    } else if (elem.height >= elem.width) {
+        elem.style.width = '100%';
+    }
+})
---
+++ TalkingUADev/wwwroot/js/scriptViewLiked.js
@@ -0,0 +1,10 @@
+let posts = document.querySelectorAll('.post img');
+posts.forEach((elem) => {
+    if (elem.width / elem.height >= 1.162) {
+        elem.style.width = '272px';
+    } else if (elem.width / elem.height < 1.162) {
+        elem.style.height = '234px';
+        elem.style.position = 'relative';
+        elem.style.top = '2px';
+    }
+})
